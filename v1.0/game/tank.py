132e8f8974154455a52f477a9aca27568fa36af9
rok <ljubeek>
2013-02-13 20:32:29 +0100

    dodan še en player, da se štimat gumbe,  pozicijo

--- v1.0/game/tank.py
+++ v1.0/game/tank.py
@@ -8,16 +8,20 @@ from .screen import *
 from .maps import *
 
 class Tank(pyglet.sprite.Sprite):
-    def __init__(self, *args, **kwargs):
+    def __init__(self,key1= None,key2 = None,key3 = None,x = None, *args,**kwargs):
         super().__init__(img = resources.tank_image,*args,**kwargs)
-        self.x = 50
+
+        self.key1 = key1
+        self.key2 = key2
+        self.key3 = key3
+        self.x = x
         self.y = landscape.height
         self.angle = 45
         self.hose = pyglet.sprite.Sprite(img=resources.hose_image)
         self.hose.x = self.x + self.width/2 
         self.hose.y = self.y + self.hose.width
         self.hose.rotation = self.angle
-        self.force = 500
+        self.force = 600
         self.rotate_speed = 50
         self.key_handler = key.KeyStateHandler()
 
@@ -63,18 +67,18 @@ class Tank(pyglet.sprite.Sprite):
                     
 
     def rotate(self, dt):
-        if(self.key_handler[key.UP]):
+        if(self.key_handler[self.key2]):
             self.hose.rotation -= self.rotate_speed * dt
             if(self.hose.rotation < -90 and self.hose.rotation > -270):
                 self.hose.rotation = -90
-        elif(self.key_handler[key.DOWN]):
+        elif(self.key_handler[self.key3]):
             self.hose.rotation += self.rotate_speed * dt
             if(self.hose.rotation > 90 and self.hose.rotation < 270):
                 self.hose.rotation = 90
 
 
     def keys(self):
-        if(self.key_handler[key.SPACE]):
+        if(self.key_handler[self.key1]):
             self.shoot()
     
         
--- v1.0/main.py
+++ v1.0/main.py
@@ -2,9 +2,8 @@ import pyglet
 from game import bullet
 from game import resources
 from game import tank
-
+from pyglet.window import key
 from game.maps import *
-
 from game.screen import *
 
 
@@ -14,25 +13,27 @@ from game.screen import *
 def on_draw():
     game_window.clear()
     landscape.draw()
-
-    
     test.draw()
+    test2.draw()
    
 def update(dt):
-
     test.update(dt)
+    test2.update(dt)
     
 
 
-test = tank.Tank()
+test = tank.Tank(key1 = key.SPACE, key2  =key.UP, key3=key.DOWN,x = 50)
+test2 = tank.Tank(key1 = key.D, key2  =key.W, key3=key.S,x = 450)
 test.shoot()
+test2.shoot()
 
 game_window.push_handlers(test.key_handler)
+game_window.push_handlers(test2.key_handler)
 
 
 
 
 
 if(__name__== '__main__'):
-    pyglet.clock.schedule_interval(update, 1/120)
+    pyglet.clock.schedule_interval(update, 1/60)
     pyglet.app.run()
